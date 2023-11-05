ddc9e96cae40ab1c8111fb027703f2d1d0766cf0
Mohammad Salam <m.salam@houya.io>
2023-11-05 19:07:50 +0100

    welcome page

--- resources/js/components/Pages/Welcome.vue
+++ resources/js/components/Pages/Welcome.vue
@@ -47,3 +47,46 @@
 </template>
 
 
+<script>
+import { computed, reactive, ref, getCurrentInstance, watchEffect } from "vue";
+import AppLayout from "@/components/Layouts/AppLayout.vue"
+export default {
+    name:"Welcome",
+    components:{
+        AppLayout,
+    },
+    setup() {
+      const cards = [
+        {id:1 ,"img":"assets/images/cards/card1.jpg"},
+        {id:2 ,"img":"assets/images/cards/card2.jpg"},
+        {id:3 ,"img":"assets/images/cards/card3.webp"},
+        {id:4 ,"img":"assets/images/cards/card4.jpg"},
+        {id:5 ,"img":"assets/images/cards/card5.jpg"},
+        {id:6 ,"img":"assets/images/cards/card6.jpg"},
+        {id:7 ,"img":"assets/images/cards/card7.jpg"},
+        {id:8 ,"img":"assets/images/cards/card8.webp"},
+        {id:9 ,"img":"assets/images/cards/card9.jpg"},
+        {id:10,"img":"assets/images/cards/card10.png"},
+        {id:11,"img":"assets/images/cards/card11.webp"},
+        {id:12,"img":"assets/images/cards/card12.webp"},
+      ]
+
+      const showDialog = ref(false);
+
+      const openModal = (card)=> {
+        console.log(card.img)
+        showDialog.value = true;
+      }
+      const closeModal = ()=> {
+        showDialog.value = false;
+      }
+        
+      return {
+          cards,
+          showDialog,
+          openModal,
+          closeModal
+      };
+    },
+}
+</script>
